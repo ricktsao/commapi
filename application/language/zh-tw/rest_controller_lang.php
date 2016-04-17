af07a4c42f5c0f273f51761567b3ee43805368f7
claire <myinfo.huang@gmail.com>
2016-04-17 21:41:19 +0800

    第一版

---
+++ application/language/zh-tw/rest_controller_lang.php
@@ -0,0 +1,16 @@
+<?php
+/*
+ * English language
+ */
+
+$lang['text_rest_invalid_api_key'] = '無效的 API key %s'; // %s is the REST API key
+$lang['text_rest_invalid_credentials'] = 'Invalid credentials';
+$lang['text_rest_ip_denied'] = 'IP 被拒絕訪問';
+$lang['text_rest_ip_unauthorized'] = 'IP 並未授權';
+$lang['text_rest_unauthorized'] = '無授權';
+$lang['text_rest_ajax_only'] = 'Only AJAX requests are allowed';
+$lang['text_rest_api_key_unauthorized'] = '您的API Key並沒有授權';
+$lang['text_rest_api_key_permissions'] = '您的API Key並沒有足夠的授權';
+$lang['text_rest_api_key_time_limit'] = '您的API Key訪問次數已超過今日的上限，請確認';
+$lang['text_rest_unknown_method'] = 'Unknown method';
+$lang['text_rest_unsupported'] = 'Unsupported protocol';
