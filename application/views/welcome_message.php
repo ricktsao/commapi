fba2bdad037e6c04b489e5acda5c480de2fe7a18
Rick <ricktsao@gmail.com>
2016-05-07 15:50:26 +0800

    api doc update

--- application/views/welcome_message.php
+++ application/views/welcome_message.php
@@ -602,7 +602,6 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 	</table>
 </div>
 
-
     </div>
 
     <p class="footer">Page rendered in <strong>{elapsed_time}</strong> seconds. <?php //echo  (ENVIRONMENT === 'development') ?  'CodeIgniter Version <strong>' . CI_VERSION . '</strong>' : '' ?></p>
