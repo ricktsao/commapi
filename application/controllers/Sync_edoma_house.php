f514fe91a9d473eaf159b2590d5fd54b65ea7d7c
Claire <claire.huang@chupei.com.tw>
2017-01-22 18:47:02 +0800

    fix

--- application/controllers/Sync_edoma_house.php
+++ application/controllers/Sync_edoma_house.php
@@ -1,19 +1,19 @@
 <?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
-class Sync_edoma_house extends CI_Controller {	
-	
-	function __construct() 
+class Sync_edoma_house extends CI_Controller {
+
+	function __construct()
 	{
-		parent::__construct();	  
+		parent::__construct();
 		$this->load->database();
 	}
-	
-	
+
+
 	public function index()
-	{		
-	
-	}	
-	
+	{
+
+	}
+
 	/**
 	 * 查詢由edoma平台新增的資料
 	**/
@@ -25,7 +25,19 @@ class Sync_edoma_house extends CI_Controller {
 
 		echo json_encode($edoma_list['data']);
 	}
-	
+
+
+    /**
+     * 查詢由edoma平台新增的資料
+    **/
+    public function getEdomaHouseToRent()
+    {
+        $comm_id = tryGetData('comm_id', $_POST, NULL);
+        $condition = "comm_id = '".$comm_id."' and edoma_sn > 0 And client_sync = 0 ";
+        $edoma_list = $this->it_model->listData( "house_to_rent" , $condition );
+
+        echo json_encode($edoma_list['data']);
+    }
 
 	/**
 	 * 查詢由edoma平台新增的資料
@@ -34,7 +46,7 @@ class Sync_edoma_house extends CI_Controller {
 	{
 	/**	$_POST = array('comm_id' => 'qazWSXed'
     					,'edoma_house_to_sale_sn' => 5 );
-    	
+
 		dprint('_POST _POST');
 		dprint($_POST);
 	**/
@@ -47,27 +59,46 @@ class Sync_edoma_house extends CI_Controller {
 		echo json_encode($edoma_list["data"]);
 	}
 
+    /**
+     * 查詢由edoma平台新增的資料
+    **/
+    public function getEdomaHouseToRentPhoto()
+    {
+    /** $_POST = array('comm_id' => 'qazWSXed'
+                        ,'edoma_house_to_rent_sn' => 5 );
+
+        dprint('_POST _POST');
+        dprint($_POST);
+    **/
+        $comm_id = tryGetData('comm_id', $_POST, NULL);
+        $edoma_house_to_rent_sn = tryGetData('edoma_house_to_rent_sn', $_POST, NULL);
+        $condition = "client_sync = 0 aNd edoma_house_to_rent_sn =".$edoma_house_to_rent_sn;
+        $edoma_list = $this->it_model->listData( "house_to_rent_photo" , $condition );
+
+
+        echo json_encode($edoma_list["data"]);
+    }
 
 	public function updateEdomaContent()
-	{	
+	{
 		foreach( $_POST as $key => $value )
 		{
-			$edit_data[$key] = $this->input->post($key,TRUE);			
+			$edit_data[$key] = $this->input->post($key,TRUE);
 		}
-		$edit_data["content"] = $this->input->post("content");	
-		
+		$edit_data["content"] = $this->input->post("content");
+
 		$arr_data = array
-		(   
-			/*  "client_sn" => tryGetData("sn",$edit_data,NULL)	
-			, "title" => tryGetData("title",$edit_data)	
+		(
+			/*  "client_sn" => tryGetData("sn",$edit_data,NULL)
+			, "title" => tryGetData("title",$edit_data)
 			, "brief" => tryGetData("brief",$edit_data)
-			, "brief2" => tryGetData("brief2",$edit_data)	
-			, "id" => tryGetData("id",$edit_data,NULL)	
+			, "brief2" => tryGetData("brief2",$edit_data)
+			, "id" => tryGetData("id",$edit_data,NULL)
 			, "content_type" => tryGetData("content_type",$edit_data)
 			, "start_date" => tryGetData("start_date",$edit_data,NULL)
 			, "end_date" => tryGetData("end_date",$edit_data,NULL)
 			, "forever" => tryGetData("forever",$edit_data,0)
-			, "launch" => tryGetData("launch",$edit_data,0)			
+			, "launch" => tryGetData("launch",$edit_data,0)
 			, "hot" => tryGetData("hot",$edit_data,0)
 			, "del" => tryGetData("del",$edit_data,0)
 			, "sort" => tryGetData("sort",$edit_data,500)
@@ -79,15 +110,15 @@ class Sync_edoma_house extends CI_Controller {
 			,*/
 			"client_sync" =>  1
 		);
-		
+
 		if($this->it_model->updateData( "web_menu_content" , $arr_data, "sn = '".$edit_data["server_sn"]."' AND comm_id = '".tryGetData("comm_id",$edit_data)."' " ))
 		{
 			echo '1';
 		}
-		else 
+		else
 		{
 			echo '0';
 		}
 	}
-	
+
 }
